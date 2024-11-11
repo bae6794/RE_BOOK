a6364cb54af1bf0cdac2d4ce54adc7a15f7b71ac
Chani's <psc0729@naver.com>
2024-11-11 17:09:51 +0900

    init 중

---
+++ src/main/java/com/re_book/ReBookApplication.java
@@ -0,0 +1,13 @@
+package com.re_book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/re_book/ReBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.re_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

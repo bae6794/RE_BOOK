d3d12e69e088f6449fab685c8f33ae176022cfa9
Chani's <psc0729@naver.com>
2024-11-14 16:41:39 +0900

    feat : 유저컨트롤러 로그인 레스트컨트롤러 시연 구현

---
+++ src/main/java/com/re_book/common/auth/TokenUserInfo.java
@@ -0,0 +1,15 @@
+//package com.re_book.common.auth;
+//
+//import com.playdata.orderservice.user.entity.Role;
+//import lombok.*;
+//
+//@Setter @Getter @ToString
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Builder
+//public class TokenUserInfo {
+//
+//    private String email;
+//    private Role role;
+//
+//}
